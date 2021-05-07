688c337107b51627eaebbda5697d5b98bbe18e1f
dakshgarera <77096274+dakshgarera@users.noreply.github.com>
2021-05-07 19:41:08 +0530

    Add files via upload

---
+++ polygon.js
@@ -0,0 +1,19 @@
+class Polygon{
+    constructor(x,y,width,height){
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("images/polygon.png");
+        World.add(world, this.body);
+    }
+    display(){
+        push ()
+rectMode()
+        pop ()
+    }
+}
